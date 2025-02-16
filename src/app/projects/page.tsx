bb64fc57fd3f30c8499e3a7a7e2c44cc150444ea
Arvin Saguisa <arvinsaguisa@gmail.com>
2025-02-16 23:39:44 +0800

    projects page content update

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,7 @@ export default function ProjectsPage() {
         <p className="text-gray-600 dark:text-gray-300 mb-6">
             Here are some of the projects I have worked on, showcasing my skills in web development, 
             software testing, and UI/UX design. Each project highlights different technologies and 
-            problem-solving approaches I've used.
+            problem-solving approaches I have used.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {projectDetails.map((project) => (
