27e9e1b85bf12fb58ecdfe87b972aeb37a6a2210
Arvin-tech <arvinsaguisa@gmail.com>
2025-04-01 14:35:00 +0800

    home - skills section grid changes

--- src/app/components/SkillsSnapshot.tsx
+++ src/app/components/SkillsSnapshot.tsx
@@ -27,14 +27,21 @@ const Skills = () => {
           Skills <span className="text-blue-400">Snapshot</span>
         </h2>
 
-        <ul className="grid grid-cols-2 sm:grid-cols-2 py-6 mt-12 border-primary-800 md:grid-cols-4 gap-8 ml-20">
+        <ul className="grid grid-cols-4 gap-4 py-6 mt-12 border-primary-800 ml-4 sm:ml-0">
           {skills.map(({ name, image }) => (
-            <li key={name} className="flex items-center gap-2">
-              <Image className="w-6 h-6" src={image} alt={name} width={24} height={24} />
-              <p className="text-base text-primary-300">{name}</p>
+            <li key={name} className="flex flex-col items-center gap-1 sm:gap-2">
+              <Image
+                className="w-5 h-5 sm:w-6 sm:h-6 md:w-8 md:h-8"
+                src={image}
+                alt={name}
+                width={32}
+                height={32}
+              />
+              <p className="text-xs sm:text-sm md:text-base text-primary-300">{name}</p>
             </li>
           ))}
         </ul>
+
       </div>
     </section>
   );
